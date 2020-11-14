3655c87285736bb5433798ce6e8920ed06786308
Romeo de Wit <romeo@classicremedies.co.uk>
2020-11-14 15:37:12 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "removing all ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 validated_url = open(url).read
 cocktailIngredients = JSON.parse(validated_url)
-ingredientsList = ["Campari", "Orange Juice", "Chartreuse Green", "Chartreuse Yellow", "Sugar syrup", "Pimento dram", "Angostura Bitters", "Peychauds Bitters", "Rye", "Lillet Blanc". "Absinth"]
+ingredientsList = ["Campari", "Orange Juice", "Chartreuse Green", "Chartreuse Yellow", "Sugar syrup", "Pimento dram", "Angostura Bitters", "Peychauds Bitters", "Rye", "Lillet Blanc", "Absinth"]
 
 cocktailIngredients["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient.values.first)
