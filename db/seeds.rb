6179b4f0cd7248d470ace3ab9ecac9d8fdea2e30
Romeo de Wit <romeo@classicremedies.co.uk>
2020-11-14 15:24:27 +0000

    layer test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,16 @@ puts "removing all ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 validated_url = open(url).read
 cocktailIngredients = JSON.parse(validated_url)
+ingredientsList = ["Campari", "Orange Juice", "Chartreuse Green", "Chartreuse Yellow", "Sugar syrup", "Pimento dram", "Angostura Bitters", "Peychauds Bitters", "Rye", "Lillet Blanc". "Absinth"]
 
 cocktailIngredients["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient.values.first)
   puts "adding ingredient"
+end
+
+ingredientsList.each do |ingredient|
+  Ingredient.create(name: ingredient)
+  puts "adding even more ingredients"
 end
 
 puts "all ingredients added to Database"
